bd496a332b15a3b97d8765f3f36ff3e82e40a25e
suraj-web <shakyasuraj595@gamil.com>
2025-03-21 16:58:42 +0530

    add

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,89 +16,93 @@ export default function Navbar() {
             </nav>
             <div className="sec-nav">
                 <ul>
-                    <ul className="Sec-ankerul">
-                        <li>
-                            <Link to="/account" className="Secnavanker">Account</Link>
-                            <i className="fa-solid fa-angle-down"></i>
-                        </li>
-                        <li className="Sec-anker-ul-li">
-                            <Link to="/account/savings">Saving Account</Link>
-                            <hr />
-                            <Link to="/account/salary">Salary Account</Link>
-                            <hr />
-                            <Link to="/account/current">Current Account</Link>
-                        </li>
-                    </ul>
 
-                    <hr />
+                    <div className="dispalynonecontainer">
+                        <ul className="Sec-ankerul">
+                            <li>
+                                <Link to="/account" className="Secnavanker">Account</Link>
+                                <i className="fa-solid fa-angle-down"></i>
+                            </li>
+                            <li className="Sec-anker-ul-li">
+                                <Link to="/account/savings">Saving Account</Link>
+                                <hr />
+                                <Link to="/account/salary">Salary Account</Link>
+                                <hr />
+                                <Link to="/account/current">Current Account</Link>
+                            </li>
+                        </ul>
 
-                    <ul className="Sec-ankerul">
-                        <li>
-                            <Link to="/deposit" className="Secnavanker">Deposits</Link>
-                            <i className="fa-solid fa-angle-down"></i>
-                        </li>
-                        <li className="Sec-anker-ul-li">
-                            <Link to="/deposit/fixed">Fixed Deposit</Link>
-                            <hr />
-                            <Link to="/deposit/recurring">Recurring Deposit</Link>
-                        </li>
-                    </ul>
+                        <hr />
 
-                    <hr />
+                        <ul className="Sec-ankerul">
+                            <li>
+                                <Link to="/deposit" className="Secnavanker">Deposits</Link>
+                                <i className="fa-solid fa-angle-down"></i>
+                            </li>
+                            <li className="Sec-anker-ul-li">
+                                <Link to="/deposit/fixed">Fixed Deposit</Link>
+                                <hr />
+                                <Link to="/deposit/recurring">Recurring Deposit</Link>
+                            </li>
+                        </ul>
 
-                    <ul className="Sec-ankerul">
-                        <li>
-                            <Link to="/payment" className="Secnavanker">Payments</Link>
-                            <i className="fa-solid fa-angle-down"></i>
-                        </li>
-                        <li className="Sec-anker-ul-li">
-                            <Link to="/payment/transfer">Money Transfer</Link>
-                            <hr />
-                            <Link to="/payment/loan">Overdue Loan Payment</Link>
-                            <hr />
-                            <Link to="/payment/credit-bill">Credit Card Bill Payment</Link>
-                            <hr />
-                            <Link to="/payment/contactless">Contactless Payments</Link>
-                        </li>
-                    </ul>
+                        <hr />
 
-                    <hr />
+                        <ul className="Sec-ankerul">
+                            <li>
+                                <Link to="/payment" className="Secnavanker">Payments</Link>
+                                <i className="fa-solid fa-angle-down"></i>
+                            </li>
+                            <li className="Sec-anker-ul-li">
+                                <Link to="/payment/transfer">Money Transfer</Link>
+                                <hr />
+                                <Link to="/payment/loan">Overdue Loan Payment</Link>
+                                <hr />
+                                <Link to="/payment/credit-bill">Credit Card Bill Payment</Link>
+                                <hr />
+                                <Link to="/payment/contactless">Contactless Payments</Link>
+                            </li>
+                        </ul>
 
-                    <ul className="Sec-ankerul">
-                        <li>
-                            <Link to="/credit-cards" className="Secnavanker">Cards</Link>
-                            <i className="fa-solid fa-angle-down"></i>
-                        </li>
-                        <li className="Sec-anker-ul-li">
-                            <Link to="/credit-cards">Credit Cards</Link>
-                            <hr />
-                            <Link to="/credit-cards/debit">Debit Cards</Link>
-                            <hr />
-                            <Link to="/credit-cards/prepaid">Prepaid Cards</Link>
-                            <hr />
-                            <Link to="/credit-cards/loan">Loan on Credit Card</Link>
-                        </li>
-                    </ul>
+                        <hr />
 
-                    <hr />
+                        <ul className="Sec-ankerul">
+                            <li>
+                                <Link to="/credit-cards" className="Secnavanker">Cards</Link>
+                                <i className="fa-solid fa-angle-down"></i>
+                            </li>
+                            <li className="Sec-anker-ul-li">
+                                <Link to="/credit-cards">Credit Cards</Link>
+                                <hr />
+                                <Link to="/credit-cards/debit">Debit Cards</Link>
+                                <hr />
+                                <Link to="/credit-cards/prepaid">Prepaid Cards</Link>
+                                <hr />
+                                <Link to="/credit-cards/loan">Loan on Credit Card</Link>
+                            </li>
+                        </ul>
 
-                    <ul className="Sec-ankerul">
-                        <li>
-                            <Link to="/loan-details" className="Secnavanker">Loans</Link>
-                            <i className="fa-solid fa-angle-down"></i>
-                        </li>
-                        <li className="Sec-anker-ul-li">
-                            <Link to="/loan-details/personal">Personal Loan</Link>
-                            <hr />
-                            <Link to="/loan-details/home">Home Loans</Link>
-                            <hr />
-                            <Link to="/loan-details/car">Car Loan</Link>
-                            <hr />
-                            <Link to="/loan-details/smart">Smart Loans</Link>
-                        </li>
-                    </ul>
+                        <hr />
 
-                    <hr />
+                        <ul className="Sec-ankerul">
+                            <li>
+                                <Link to="/loan-details" className="Secnavanker">Loans</Link>
+                                <i className="fa-solid fa-angle-down"></i>
+                            </li>
+                            <li className="Sec-anker-ul-li">
+                                <Link to="/loan-details/personal">Personal Loan</Link>
+                                <hr />
+                                <Link to="/loan-details/home">Home Loans</Link>
+                                <hr />
+                                <Link to="/loan-details/car">Car Loan</Link>
+                                <hr />
+                                <Link to="/loan-details/smart">Smart Loans</Link>
+                            </li>
+                        </ul>
+
+                        <hr />
+                    </div>
+
 
                     <LoginComponent />
 
